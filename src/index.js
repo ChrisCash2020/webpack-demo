f5749f7bdf7fc3863870ea709fb69231f948f27f
ChrisCash2020 <81109855+ChrisCash2020@users.noreply.github.com>
2021-06-16 22:00:54 -0500

    Output management tut

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print';
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, now imported by this script
+  const btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
   return element;
 }
 
